4ada79f0037db5291baba13c0c00ceca61afed5b
JairDevsen <151375607+JairDevsen@users.noreply.github.com>
2026-08-10 19:05:12 -0300

    Change in files

--- POO/Veterinaria/src/main/java/com/example/demo/repository/PetOwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.PetOwner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetOwnerRepository extends JpaRepository<PetOwner, Long> {
-}
--- POO/Veterinaria/src/main/java/com/example/demo/repository/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
